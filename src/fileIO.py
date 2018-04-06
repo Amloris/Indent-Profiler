2415ce0845fe3995fc5cc7c95b7bd5959582e73c
Amloris <aaronrob@ksu.edu>
2018-04-06 13:55:38 -0500

    Added file loading, function docstrings, and error handling

--- src/fileIO.py
+++ src/fileIO.py
@@ -9,26 +9,32 @@ FRA
 March 2018
 -------------------------------------------------------------------------------
 File Description:
-    Provides file handling utilities for the Indent Profiler project. Allows 
+    Provides file handling utilities for the Indent Profiler project. Allows
 for user specified directory and file selection, file loading and saving, and
 path building.
 
 Changelog:
-04/08/18 - Updated header, added InitFileWindow()
+04/05/18 - Updated header, added InitFileWindow(), changed deault file paths.
+04/06/18 - Improved function docstrings. Added LoadData(), SaveData(), 
+           LoadSlicedData(), and HeaderInfo class.
 -------------------------------------------------------------------------------
 """
-
-
 
 #Libraries
 import numpy as np                 #Math
 import os                          #Input/Output
+import sys                         #Input/Output
 from Tkinter import Tk,TclError    #Input/Output
 import tkFileDialog                #Input/Output
 
 
+'''File Systems'''
+'''-------------------------------------------------------------------------'''
+
 def GetDir():
-    '''Opens a window that allows a user to select a directory.'''
+    '''Opens a window that allows a user to select a directory.
+       Returns the directory path.
+    '''
     
     #Set Window Attributes
     InitFileWindow()
@@ -46,7 +52,9 @@ def GetDir():
     return dir_name
 
 def GetFile():
-    '''Opens a window that allows a user to select a specific file.'''
+    '''Opens a window that allows a user to select a specific file.
+       Returns the file path.
+    '''
     
     #Set Window Attributes
     InitFileWindow()
@@ -58,13 +66,17 @@ def GetFile():
     print "Select a data file:"
     fname = str((tkFileDialog.askopenfilename(initialdir="../",               \
                  title = 'Select Data File', filetypes=[("CSV","*.csv"),      \
-                 ("Text","*.txt")])) or default_file)
+                 ("Text","*.txt")])) or default_file)  
     fname = os.path.normpath(fname)
     print fname, '\n'
 
     return fname
 
 def InitFileWindow():
+    '''Called before GetFile() and GetDir() in order to hide hidden elements
+       on UNIX systems. The user has the option to toggle file hidding.
+    '''
+    
     root = Tk()
     root.withdraw()
 
@@ -90,6 +102,107 @@ def InitFileWindow():
         
     return root
     
+
+'''File Loading/Saving'''
+'''-------------------------------------------------------------------------'''
+
+def LoadData(fname, verify=True):
+    '''
+    Loads single segment scans and optionally checks for data integrity.
+    
+    Inputs
+    ------
+    fname : str
+        The path to the file. The extension must be .txt or .csv to be loaded    
+    verify : bool, optional
+        If True, data integrity will be checked [default].
+    
+    Outputs
+    -------
+    header : class
+        Header block data will be stored in a class for easy access.
+    data: ndarray
+        Scanner data is loaded into a [nxm] array. n = pts/rev, m = 800
+        
+    Notes
+    -----
+    A file extension check is provided in this function as a default. This 
+    function will be used in multiple projects where file extensions are not
+    guaranteed to be verfied prior to the LoadData() function call.
+    '''
+    
+    #System Parameters
+    cols_expected = 800       #Number of axial data points for the scanner
+
+    #Set Extension
+    filename, file_ext = os.path.splitext(fname)
+    if (file_ext == '.txt'):
+        delim = None
+    elif (file_ext == '.csv'):
+        delim = ','
+    else:
+        sys.exit("TERMINATE:LOAD_DATA:INVALID_EXTENSION")
+
+    #Load Header
+    header_length = 6
+    header_index = np.arange(0, header_length)        #Header data rows
+
+    with open(fname,'r') as fin:
+        for i, line in enumerate(fin):
+            if i in header_index:
+                line_val = line.strip().split(delim)[-1]
+                if i == 0: wire_profile     = str(line_val) 
+                if i == 1: x_location       = float(line_val)
+                if i == 2: total_sample_len = float(line_val)    
+                if i == 3: pts_per_rev      = int(line_val)                   
+                if i == 4: data_state       = str(line_val)
+                if i == 5: time_stamp       = str(line_val)
+
+    header = HeaderInfo(wire_profile, x_location, total_sample_len, \
+                         pts_per_rev, data_state, time_stamp)
+
+    #Load Data
+    data = np.loadtxt(fname, delimiter=delim, skiprows=header_length)
+
+    #Check Data Validity
+    if verify:
+        flag = 0
+        if np.shape(data)[0] != header.pts_per_rev:
+            flag = 1
+            print "ERROR: Expected %i rows, got %i rows" \
+                  %(header.pts_per_rev, np.shape(data)[0])
+        if np.shape(data)[1] != cols_expected:
+            flag = 1
+            print "ERROR: Expected %i cols, got %i cols" \
+                  %(cols_expected, np.shape(data)[1])
+        if flag == 1:
+            sys.exit("TERMINATE:LOAD_DATA:INVALID_ARRAY_BOUNDS")
+        else:
+            return data, header   
+    else:
+        return data, header
+
+def SaveData():
+    return 0
+
+def LoadSplicedData():
+    return 0
+
+class HeaderInfo():
+    '''Used to store the header data of scan files.'''
+    
+    def __init__(self, wire_profile, x_location, total_sample_length,
+				 pts_per_rev, data_state, time_stamp):
+        self.wire_profile = wire_profile
+        self.x_location = x_location
+        self.total_sample_length = total_sample_length
+        self.pts_per_rev = pts_per_rev
+        self.data_state = data_state
+        self.time_stamp = time_stamp
+
+
+'''Misc'''
+'''-------------------------------------------------------------------------'''
 
 def VerifyFileNames(dir_name):
     '''Verfies that the files follow the documented naming conventions.
@@ -135,6 +248,8 @@ if __name__ == "__main__":
     InitFileWindow()    
 
     GetDir()
-    GetFile()
+    temp = GetFile()
+    
+    LoadData(temp)
     
     #root.mainloop()
